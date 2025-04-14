afb3a49835b62d78edd5774b0d995c9587b57ea3
wrightdj99 <wrightdj99@gmail.com>
2025-04-13 23:45:12 -0500

    Got everything all ready to go.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,20 +1,29 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from .models import Question
-
 
 # Create your views here.
 
 def index(request):
     latest_questions = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-    return HttpResponse(output)
+    context = {
+        "latest_questions":latest_questions
+    }
+    return render(request, "polls/index.html", context)
 def detail(request, question_id):
-    return HttpResponse("This is the detail view of the question: %s" % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 def results(request, question_id):
-    return HttpResponse("This is the results page of question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
 def vote(request, question_id):
-    return HttpResponse("This is the vote page for question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question':question, "error_message":"Please select a choice"})
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return render(request, "polls/results.html", {"question":question})
