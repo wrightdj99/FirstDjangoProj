380440008e7896b3b44ebd7a04c04d916da363c7
wrightdj99 <wrightdj99@gmail.com>
2025-04-21 11:52:54 -0500

    Priming for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8qec!unv-fr)gza&4j*)drsdsmd7gcso%+2vd#56ri4^=-#m39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
