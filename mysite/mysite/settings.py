8cb402865ef0a0417febeedee03572ac92b06776
wrightdj99 <wrightdj99@gmail.com>
2025-04-21 14:58:33 -0500

    Changing up the admin page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+
+    "admin_interface",
+    "colorfield",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
